8d077148a2a1740dc19eb104954136f051d46c3b
Christian Joseph <christianrjo@gmail.com>
2014-02-17 16:05:07 -0800

    refactor tests

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -1,21 +1,25 @@
-# require_relative 'lineitem'
 require_relative '../lib/receipt'
 
 describe "output" do
 
   it "returns output 1" do
-    input1 = Receipt.new('data/input1.txt')
+    array_of_line_items = File.readlines('data/input1.txt')
+    lineitems = array_of_line_items.map{|text| LineItem.new(text.chomp("\n"))} 
+    input1 = Receipt.new(lineitems)
     expect(input1.to_s).to eql("1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 29.83")
   end
 
   it "returns output 2" do
-    input2 = Receipt.new('data/input2.txt')
+    array_of_line_items = File.readlines('data/input2.txt')
+    lineitems = array_of_line_items.map{|text| LineItem.new(text.chomp("\n"))} 
+    input2 = Receipt.new(lineitems)
     expect(input2.to_s).to eql("1 imported box of chocolates: 10.50\n1 imported bottle of perfume: 54.65\nSales Taxes: 7.65\nTotal: 65.15")
   end
 
   it "returns output 3" do
-    input3 = Receipt.new('data/input3.txt')
+    array_of_line_items = File.readlines('data/input3.txt')
+    lineitems = array_of_line_items.map{|text| LineItem.new(text.chomp("\n"))} 
+    input3 = Receipt.new(lineitems)
     expect(input3.to_s).to eql("1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 9.75\n1 box of imported chocolates: 11.85\nSales Taxes: 6.70\nTotal: 74.68")
   end
-
 end
--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -1,33 +1,44 @@
 require_relative '../lib/receipt'
 require_relative '../lib/lineitem'
 
-describe 'Receipt' do
 
-  let(:input_file) { 'data/input1.txt' }
-  let(:receipt) {  Receipt.new(input_file) }
-
-  describe '#initialize' do
-    it 'should not instantiates LineItem' do
-      expect{Receipt.new}.to raise_error(ArgumentError)
-    end
+describe Receipt, '#initialize' do
+  it 'should not instantiates Receipt' do
+    expect{Receipt.new}.to raise_error(ArgumentError)
   end
-
-  describe '#to_s' do
-    it 'prints updated receipt' do
-      expect(receipt.to_s).to eql("1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 29.83")
-    end
-  end
-
-  describe '#total' do
-    it 'calculates total of all line items without tax' do
-      expect(receipt.total).to eql(29.83)
-    end
-  end
-
-  describe '#total_tax' do
-    it 'calculates total tax for all items' do
-      expect(receipt.total_tax).to eql(1.5)
-    end
-  end
-
 end
+
+describe Receipt, '#to_s' do
+  it 'prints updated receipt' do
+    lineitem = double('LineItem')
+    allow(lineitem).to receive(:to_s)
+    allow(lineitem).to receive(:total_with_tax).and_return(0.05)
+    allow(lineitem).to receive(:tax).and_return(0.10)
+    
+    result = Receipt.new([lineitem]).to_s
+    expect(result).to eql "\nSales Taxes: 0.10\nTotal: 0.05"
+  end
+end
+
+describe Receipt, '#total' do
+  it 'calculates total of all line items without tax' do
+    line_items = [
+      double(total_with_tax: 7),
+      double(total_with_tax: 8),
+    ]
+    result = Receipt.new(line_items).total
+    expect(result).to eql 15.0
+  end
+end
+
+describe Receipt, '#total_tax' do
+  it 'returns the summation of tax for line items' do
+    line_items = [
+      double(tax: 5),
+      double(tax: 2),
+    ]
+    result = Receipt.new(line_items).total_tax
+    expect(result).to eql 7
+  end 
+end
+
